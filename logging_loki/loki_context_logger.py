16db23f6a19dc98d4059191ee1a38667dfce13aa
Matthias Dillen <matthias.dillen@inuits.eu>
2024-05-08 14:04:37 +0200

    log_context per log to prevent bugs with threading

--- logging_loki/loki_context_logger.py
+++ logging_loki/loki_context_logger.py
@@ -5,32 +5,31 @@ from .loki_logger import LokiLogger
 class LokiContextLogger:
     def __init__(self, logger: LokiLogger):
         self.logger = logger
-        self.log_context = LogContext()
 
-    def _log(self, level, message, code=None, exc_info=None):
-        self.log_context.set_message(message)
+    def _log(self, level, message, code=None, log_context: LogContext | None = LogContext(), exc_info=None):
+        log_context.set_message(message)
         tags = {"message_code": code}
-        tags.update(self.log_context.get_tags())
+        tags.update(log_context.get_tags())
         log_func = getattr(self.logger, level)
         if level == "exception":
-            log_func(self.log_context.to_string(), tags, exc_info=exc_info)
+            log_func(log_context.to_string(), tags, exc_info=exc_info)
         else:
-            log_func(self.log_context.to_string(), tags)
+            log_func(log_context.to_string(), tags)
 
-    def debug(self, message, code=None):
-        self._log('debug', message, code)
+    def debug(self, message, code=None, log_context=None):
+        self._log('debug', message, code, log_context)
 
-    def info(self, message, code=None):
-        self._log('info', message, code)
+    def info(self, message, code=None, log_context=None):
+        self._log('info', message, code, log_context)
 
-    def warning(self, message, code=None):
-        self._log('warning', message, code)
+    def warning(self, message, code=None, log_context=None):
+        self._log('warning', message, code, log_context)
 
-    def error(self, message, code=None):
-        self._log('error', message, code)
+    def error(self, message, code=None, log_context=None):
+        self._log('error', message, code, log_context)
 
-    def critical(self, message, code=None):
-        self._log('critical', message, code)
+    def critical(self, message, code=None, log_context=None):
+        self._log('critical', message, code, log_context)
 
-    def exception(self, message, exc_info=None, code=None):
-        self._log('exception', message, code, exc_info)
+    def exception(self, message, exc_info=None, code=None, log_context=None):
+        self._log('exception', message, code, log_context, exc_info)
